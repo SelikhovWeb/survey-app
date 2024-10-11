0ae9f42a7a2b5c7c65f7aa53fe721c1b4b9a8a60
Misha <selikhov.web@gmail.com>
2024-10-11 11:49:48 +0200

    header, minor bg fix

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -22,7 +22,7 @@ const Header = () => {
   return (
     <div
       className={classNames(styles.header, {
-        [styles.darkTheme]: isDarkThemeEnabled,
+        [styles.darkThemeHeader]: isDarkThemeEnabled,
       })}
     >
       {shouldDisplayBackButtonInHeader && (
